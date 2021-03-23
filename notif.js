9ae917b6a1965c3f52fb16079acdaedaee74fa4f
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-03-23 15:27:36 -0400

    fix link & add short wait

--- notif.js
+++ notif.js
@@ -1,41 +1,51 @@
 const axios = require('axios').default;
 const config = require('./config');
+const fs = require('fs');
 
-module.exports.sendNotif = (name, id, sizeBytes, hostname, ip) => {
-    if (config.discord.on) {
-        axios.post(`${config.discord.webhook}`, {
-            embeds: [{
-                title: "New Upload",
-                description: 'New File Uploaded to the Feck Files Drive',
-                color: "3066993",
-                fields: [{
-                    name: "File Name",
-                    value: `${name}`,
-                    inline: true
-                }, {
-                    name: "File ID",
-                    value: `${id}`,
-                    inline: true
-                }, {
-                    name: "Access URL",
-                    value: `[Click me](https://${hostname}/uploads/${id}/${name})`,
-                    inline: true
-                }, {
-                    name: "File Size",
-                    value: `${formatSize(sizeBytes)}`,
-                    inline: true
-                }, {
-                    name: "Uploader IP",
-                    value: `${ip}`,
-                    inline: true
-                }, {
-                    name: "Timestamp",
-                    value: `${new Date().toUTCString()}`,
-                    inline: true
-                }]
-            }],
-        }).catch(e => console.log("Error occurred. Could not send notification"));
-    }
+module.exports.sendNotif = (name, id, hostname, ip) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            if (config.discord.on) {
+                let sizeBytes = fs.statSync(`./uploads/${id}/${name}`).size;
+                console.log(sizeBytes);
+                axios.post(`${config.discord.webhook}`, {
+                    embeds: [{
+                        title: "New Upload",
+                        description: 'New File Uploaded to the Feck Files Drive',
+                        color: "3066993",
+                        fields: [{
+                            name: "File Name",
+                            value: `${name}`,
+                            inline: true
+                        }, {
+                            name: "File ID",
+                            value: `${id}`,
+                            inline: true
+                        }, {
+                            name: "Access URL",
+                            value: `[Click me](https://${hostname}/uploads/${id}/${encodeURIComponent(name)})`,
+                            inline: true
+                        }, {
+                            name: "File Size",
+                            value: `${formatSize(sizeBytes)}`,
+                            inline: true
+                        }, {
+                            name: "Uploader IP",
+                            value: `${ip}`,
+                            inline: true
+                        }, {
+                            name: "Timestamp",
+                            value: `${new Date().toUTCString()}`,
+                            inline: true
+                        }]
+                    }],
+                }).then(() => resolve()).catch(e => { reject(e) });
+            }
+            else {
+                resolve();
+            }
+        }, 500);
+    })
 }
 
 function formatSize(number) {
