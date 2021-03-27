0d7b1e924ec502167cd594d32c8199715d33bfb7
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-03-27 15:53:00 -0400

    remove timeout

--- notif.js
+++ notif.js
@@ -1,64 +1,46 @@
 const axios = require('axios').default;
 const config = require('./config');
-const fs = require('fs');
 
-module.exports.sendNotif = (name, id, hostname, ip) => {
+module.exports.sendNotif = (name, id, hostname, ip, size) => {
     return new Promise((resolve, reject) => {
-        setTimeout(function () {
-            if (config.discord.on) {
-                let sizeBytes = fs.statSync(`./uploads/${id}/${name}`).size;
-                axios.post(`${config.discord.webhook}`, {
-                    username: "Feck Files Upload Notification",
-                    avatar_url: `https://${hostname}/site/files/icon.png`,
-                    embeds: [{
-                        title: "New Upload",
-                        description: 'New File Uploaded to the Feck Files Drive',
-                        color: "3066993",
-                        fields: [{
-                            name: "File Name",
-                            value: `${name}`,
-                            inline: true
-                        }, {
-                            name: "File ID",
-                            value: `${id}`,
-                            inline: true
-                        }, {
-                            name: "Access URL",
-                            value: `[Click me](https://${hostname}/uploads/${id}/${encodeURIComponent(name)})`,
-                            inline: true
-                        }, {
-                            name: "File Size",
-                            value: `${formatSize(sizeBytes)}`,
-                            inline: true
-                        }, {
-                            name: "Uploader IP",
-                            value: `${ip}`,
-                            inline: true
-                        }, {
-                            name: "Timestamp",
-                            value: `${new Date().toUTCString()}`,
-                            inline: true
-                        }]
-                    }],
-                }).then(() => resolve()).catch(e => { reject(e) });
-            }
-            else {
-                resolve();
-            }
-        }, 500);
+        if (config.discord.on) {
+            axios.post(`${config.discord.webhook}`, {
+                username: "Feck Files Upload Notification",
+                avatar_url: `https://${hostname}/site/files/icon.png`,
+                embeds: [{
+                    title: "New Upload",
+                    description: 'New File Uploaded to the Feck Files Drive',
+                    color: "3066993",
+                    fields: [{
+                        name: "File Name",
+                        value: `${name}`,
+                        inline: true
+                    }, {
+                        name: "File ID",
+                        value: `${id}`,
+                        inline: true
+                    }, {
+                        name: "Access URL",
+                        value: `[Click me](https://${hostname}/uploads/${id}/${encodeURIComponent(name)})`,
+                        inline: true
+                    }, {
+                        name: "File Size",
+                        value: `${size}`,
+                        inline: true
+                    }, {
+                        name: "Uploader IP",
+                        value: `${ip}`,
+                        inline: true
+                    }, {
+                        name: "Timestamp",
+                        value: `${new Date().toUTCString()}`,
+                        inline: true
+                    }]
+                }],
+            }).then(() => resolve()).catch(e => { reject(e) });
+        }
+        else {
+            resolve();
+        }
     })
-}
-
-function formatSize(number) {
-    if (number >= 1024 * 1024) {
-        let mbSize = parseInt(number / (1024 * 1024));
-        return `${mbSize}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "MB";
-    }
-    else if (number >= 1024) {
-        let kbSize = parseInt(number / (1024));
-        return `${kbSize}KB`;
-    }
-    else {
-        return `${number}B`;
-    }
 }
