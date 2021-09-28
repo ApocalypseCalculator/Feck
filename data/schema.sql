5233110db20aaa01872cf4638fc74d0d64168299
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-09-28 18:51:52 -0400

    update schema

--- data/schema.sql
+++ data/schema.sql
@@ -9,22 +9,22 @@ folder TEXT NOT NULL DEFAULT "none",
 public INTEGER NOT NULL DEFAULT 1 CHECK(public = 1 OR public = 0),
 FOREIGN KEY (owner) REFERENCES users (userid),
 FOREIGN KEY (folder) REFERENCES folders (id)
-)
+);
 
 CREATE TABLE csrf(
 token TEXT NOT NULL UNIQUE,
 generated INTEGER NOT NULL
-)
+);
 
 CREATE TABLE users(
 userid TEXT PRIMARY KEY,
 username TEXT NOT NULL UNIQUE,
 password TEXT NOT NULL,
 creation INTEGER NOT NULL
-)
+);
 
 CREATE TABLE folders(
 id TEXT PRIMARY KEY,
 owner TEXT NOT NULL DEFAULT "none",
 FOREIGN KEY (owner) REFERENCES users (userid)
-)
+);
