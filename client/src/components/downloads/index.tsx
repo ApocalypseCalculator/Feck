823ce7f4edeb82d66b792eaa1deadcdb8a59110c
Patrick Lin <apocalypsecalculator@gmail.com>
2022-04-23 22:49:37 -0400

    fix table when no files

--- client/src/components/downloads/index.tsx
+++ client/src/components/downloads/index.tsx
@@ -105,54 +105,49 @@ export const Downloads = () => {
 
 function GenerateTable(props: any) {
     let session = React.useContext(SessionContext);
-    if (props.files.length == 0) {
-        return <tr><td>No files to display</td></tr>;
+    let table = props.files.map((file: any) => {
+        if (props.search === "" || file.name.toLowerCase().indexOf(props.search.toLowerCase()) > -1) {
+            if (!session.user.loggedin || (props.pubview && file.userid !== session.user.userid) || (!props.pubview && file.userid === session.user.userid)) {
+                return (<>
+                    <tr>
+                        <td className="breakname">{file.name}</td>
+                        {
+                            props.pubview ?
+                                (<td className="breakname">{
+                                    file.userid ? file.user.username : ""
+                                }</td>) : (<></>)
+                        }
+                        <td>{formatSize(parseInt(file.size))}</td>
+                        <td>{new Date(file.date).toLocaleString()}</td>
+                        <td>
+                            <button className={"btn btn-info btn-sm"}>
+                                <a href={`/uploads/?fileid=${file.id}`} style={{ color: 'azure' }} target={"_blank"} rel={"noopener noreferrer"}>Download</a>
+                            </button>
+                        </td>
+                        {
+                            !props.pubview ?
+                                (<td>{
+                                    <button type={"button"} className={"btn btn-outline-danger"} onClick={(ev) => {
+                                        ev.preventDefault();
+                                        props.opendelete(file);
+                                    }}>
+                                        <svg xmlns={"http://www.w3.org/2000/svg"} width={"16"} height={"16"} fill={"currentColor"} className={"bi bi-trash"} viewBox={"0 0 16 16"}>
+                                            <path d={"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"} />
+                                            <path fillRule={"evenodd"} d={"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"} />
+                                        </svg>
+                                    </button>
+                                }</td>) : (<></>)
+                        }
+                    </tr>
+                </>);
+            }
+        }
+    });
+    if (!table[0]) {
+        return (<tr><td>No files to display</td></tr>);
     }
     else {
-        let table = props.files.map((file: any) => {
-            if (props.search === "" || file.name.toLowerCase().indexOf(props.search.toLowerCase()) > -1) {
-                if (!session.user.loggedin || (props.pubview && file.userid !== session.user.userid) || (!props.pubview && file.userid === session.user.userid)) {
-                    return (<>
-                        <tr>
-                            <td className="breakname">{file.name}</td>
-                            {
-                                props.pubview ?
-                                    (<td className="breakname">{
-                                        file.userid ? file.user.username : ""
-                                    }</td>) : (<></>)
-                            }
-                            <td>{formatSize(parseInt(file.size))}</td>
-                            <td>{new Date(file.date).toLocaleString()}</td>
-                            <td>
-                                <button className={"btn btn-info btn-sm"}>
-                                    <a href={`/uploads/?fileid=${file.id}`} style={{ color: 'azure' }} target={"_blank"} rel={"noopener noreferrer"}>Download</a>
-                                </button>
-                            </td>
-                            {
-                                !props.pubview ?
-                                    (<td>{
-                                        <button type={"button"} className={"btn btn-outline-danger"} onClick={(ev) => {
-                                            ev.preventDefault();
-                                            props.opendelete(file);
-                                        }}>
-                                            <svg xmlns={"http://www.w3.org/2000/svg"} width={"16"} height={"16"} fill={"currentColor"} className={"bi bi-trash"} viewBox={"0 0 16 16"}>
-                                                <path d={"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"} />
-                                                <path fillRule={"evenodd"} d={"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"} />
-                                            </svg>
-                                        </button>
-                                    }</td>) : (<></>)
-                            }
-                        </tr>
-                    </>);
-                }
-            }
-        });
-        if (!table) {
-            return <tr><td>No files to display</td></tr>;
-        }
-        else {
-            return (<>{table}</>);
-        }
+        return (<>{table}</>);
     }
 }
 
