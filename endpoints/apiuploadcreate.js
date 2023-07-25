ab5aa2fbc37156c3a1f7ce2bc8cf0b6e72cdb96f
Patrick Lin <apocalypsecalculator@gmail.com>
2023-07-25 15:29:08 +0800

    add transportId in create response + fix anon private files

--- endpoints/apiuploadcreate.js
+++ endpoints/apiuploadcreate.js
@@ -16,70 +16,63 @@ module.exports.verify = function (req, res) {
 }
 
 module.exports.execute = function (req, res) {
-    res.set('Tus-Version', '1.0.0'); //hardcoding tus version :pepega:
+    res.set('Tus-Resumable', '1.0.0'); //hardcoding tus version :pepega:
     res.set('Tus-Extension', 'creation')
-    try {
-        if (req.headers["tus-resumable"] && req.headers["tus-resumable"] == "1.0.0") {
-            if (!req.headers["upload-defer-length"]) {
-                let user = null;
-                try {
-                    user = jwt.verify(req.headers.authorization, config.secrets.jwt);
-                }
-                catch { }
-                if (!req.headers['upload-length']) {
-                    return res.status(400).json({ status: 400, error: 'Missing Upload-Length header' });
-                }
-                let size = parseInt(req.headers['upload-length']);
-                if ((user && size > config.filelimit.registered) || (!user && size > config.filelimit.anon) || size <= 0) {
-                    res.set('Tus-Max-Size', user ? config.filelimit.registered : config.filelimit.anon); //reply with file limit
-                    res.status(413).json({ status: 413, error: `You have exceeded the maximum allowed size for your current session` });
-                }
-                else {
-                    if (["public", "unlisted", "private"].includes(req.body.type)) {
-                        let id = nanoid.nanoid();
-                        prisma.file.create({
+    if (req.headers["tus-resumable"] && req.headers["tus-resumable"] == "1.0.0") {
+        if (!req.headers["upload-defer-length"]) {
+            let user = null;
+            try {
+                user = jwt.verify(req.headers.authorization, config.secrets.jwt);
+            }
+            catch { }
+            if (!req.headers['upload-length']) {
+                return res.status(400).json({ status: 400, error: 'Missing Upload-Length header' });
+            }
+            let size = parseInt(req.headers['upload-length']);
+            if ((user && size > config.filelimit.registered) || (!user && size > config.filelimit.anon) || size <= 0) {
+                res.set('Tus-Max-Size', user ? config.filelimit.registered : config.filelimit.anon); //reply with file limit
+                res.status(413).json({ status: 413, error: `You have exceeded the maximum allowed size for your current session` });
+            }
+            else {
+                if ((!user && ["public", "unlisted"].includes(req.body.type)) || (user && ["public", "unlisted", "private"].includes(req.body.type))) {
+                    let id = nanoid.nanoid();
+                    prisma.file.create({
+                        data: {
+                            id: id,
+                            //name can be a max of 100 chars, name read from filename body and NOT metadata header
+                            name: (req.body.filename ?? "unknown").slice(0, 100),
+                            date: Date.now(),
+                            size: size,
+                            type: req.body.type, //public, private, or unlisted
+                            userid: user ? user.userid : null
+                        }
+                    }).then((file) => {
+                        let tusid = nanoid.customAlphabet('1234567890abcdef', 24)(); //24 digit hex
+                        prisma.upload.create({
                             data: {
-                                id: id,
-                                //name can be a max of 100 chars, name read from filename body and NOT metadata header
-                                name: (req.body.filename ?? "unknown").slice(0, 100),
-                                date: Date.now(),
-                                size: size,
-                                type: req.body.type, //public, private, or unlisted
-                                userid: user ? user.userid : null
+                                transportId: tusid,
+                                fileid: file.id,
+                                created: Date.now()
                             }
-                        }).then((file) => {
-                            let tusid = nanoid.customAlphabet('1234567890abcdef', 24)(); //24 digit hex
-                            prisma.upload.create({
-                                data: {
-                                    transportId: tusid,
-                                    fileid: file.id,
-                                    created: Date.now()
-                                }
-                            }).then(() => {
-                                res.status(201).set('Location', `/api/upload/transport/${tusid}`).json({status: 201, message: 'Successfully created upload endpoint'});
-                            }).catch((err) => {
-                                console.log(err);
-                            });
+                        }).then(() => {
+                            res.status(201).set('Location', `/api/upload/transport/${tusid}`).json({ status: 201, message: 'Successfully created upload endpoint', transportId: tusid });
                         }).catch((err) => {
                             console.log(err);
                         });
-                    }
-                    else {
-                        res.status(400).json({ status: 400, error: 'Missing upload type field' });
-                    }
+                    }).catch((err) => {
+                        console.log(err);
+                    });
                 }
-            }
-            else {
-                res.status(400).json({ status: 400, error: `Upload-Defer-Length not supported` });
+                else {
+                    res.status(400).json({ status: 400, error: 'Missing/unsupported upload type field' });
+                }
             }
         }
         else {
-            res.status(412).json({ status: 412, error: `Invalid or unsupported tus version` });
+            res.status(400).json({ status: 400, error: `Upload-Defer-Length not supported` });
         }
     }
-    catch {
-        try {
-            res.status(500).json({ error: "Internal server error" }).end();
-        } catch { }
-    };
+    else {
+        res.status(412).json({ status: 412, error: `Invalid or unsupported tus version` });
+    }
 }
