904d9f5222699884383d51a6723d17b193e16b21
Patrick Lin <apocalypsecalculator@gmail.com>
2023-07-27 17:46:03 +0800

    change upload create endpoint to use custom header instead

--- endpoints/apiuploadcreate.js
+++ endpoints/apiuploadcreate.js
@@ -32,7 +32,16 @@ module.exports.execute = function (req, res) {
                 res.set('Tus-Max-Size', user ? config.filelimit.registered : config.filelimit.anon); //reply with file limit
                 res.status(413).json({ status: 413, error: `You have exceeded the maximum allowed size for your current session` });
             }
+            else if (!req.headers["base64-meta"]) {
+                return res.status(400).json({ status: 400, error: 'Missing Base64-Meta header' });
+            }
             else {
+                try {
+                    req.body = JSON.parse(Buffer.from(req.headers["base64-meta"], 'base64').toString());
+                }
+                catch {
+                    return res.status(400).json({ status: 400, error: 'Invalid Base64-Meta header' });
+                }
                 if ((!user && ["public", "unlisted"].includes(req.body.type)) || (user && ["public", "unlisted", "private"].includes(req.body.type))) {
                     let id = nanoid.nanoid();
                     let filename = (req.body.filename ?? "unknown").replaceAll(/\s+/g, '-');
